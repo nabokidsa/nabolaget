c232676ffc721b6e1334a2b850370934559721b3
nabokidsa <131347177+nabokidsa@users.noreply.github.com>
2023-04-25 09:21:41 +0200

    Update blaa.js

--- blaa.js
+++ blaa.js
@@ -30,7 +30,7 @@ function showTask(oppgave){
     // Viser teksten
     oppgaveBeskrivelse.innerText = oppgave.beskrivelse;
      oppgaveBilde.src = `bilder/${type}/${oppgave.bilde}`;
-    oppgaveLyd.src = `lydfiler/blaa1/${oppgave.lyd}`;
+    oppgaveLyd.src = `lydfiler/blaa/${oppgave.lyd}`;
 
 }
 
